d42e3fea126ab59c2900951e3c0cab5951eb6cbb
shahd <shahd2106627@miuegypt.edu.eg>
2023-08-29 11:18:29 +0300

    test creditdto

---
+++ Esafe-Team-Project/Models/CreditCardDto.cs
@@ -0,0 +1,19 @@
+﻿using Esafe_Team_Project.Data.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace Esafe_Team_Project.Models
+{
+    public class CreditCardDto
+    {
+        public int ClientId { get; set; }
+        public CreditCardType CardType { get; set; }
+
+        [MaxLength(16, ErrorMessage = "Credit Card number cannot exceed 13 numbers")]
+        [MinLength(13, ErrorMessage = "Credit Card must be atleast 13 numbers")]
+        public string CardNumber { get; set; }
+        public string CVV { get; set; }
+        public DateTime ExpiryDate { get; set; }
+        public double BalanceAvailable { get; set; }
+        public double CardLimit { get; set; }
+    }
+}
